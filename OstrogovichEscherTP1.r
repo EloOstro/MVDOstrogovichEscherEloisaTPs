cc5a7c61c6d5122147e51c151cd291097385f547
EloOstro <eostrogovichescher@icloud.com>
2026-03-24 19:08:23 -0300

    Limpieza y join de tablas

---
+++ OstrogovichEscherTP1.r
@@ -0,0 +1,33 @@
+## PAQUETES ##
+library(readr)
+library(readxl)
+library(janitor)
+library(dplyr)
+library(haven)
+library(tidyr)
+
+internet <- "TP1/data/internet_users_world_wide.csv"
+metadata <- "TP1/data/metadata_countries_internet_users.csv"
+
+## LIMPIEZA Y JOIN DE TABLAS ##
+internet <- read_csv(
+  internet, 
+  skip = 3)
+
+metadata <- read_csv(metadata)
+
+# Transformar en formato long
+internet_long <- internet %>%
+  pivot_longer(
+    cols = "1960":"2025",
+    names_to = "year",
+    values_to = "usuarios_internet")
+
+internet_long <- internet_long %>%
+  filter(!is.na(usuarios_internet)) %>%
+  select(-...71) #VER ESTO!!!!!!
+
+# Left join
+datos <- internet_long %>%
+  left_join(metadata, by = "Country Code")
+
